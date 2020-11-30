934be905d684e7870e993f5f996d8368265b4a53
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-11-30 13:29:49 +0200

    burger-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 "use strict";
 
 $(function() {
-  $(".header__burger-menu").click(function() {
-    $(".header__menu").toggleClass("open-menu")
-  })
+  $(".header__burger-menu").on("click", function() {
+    $(".header__menu").toggleClass("open-menu");
+  });
+
+  $(".header__link-item").on("click", function(event) {
+    let anchor = $(this);
+    $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top - 30 + "px"}, 777);
+    $(".header__menu").removeClass("open-menu");
+
+    event.preventDefault();
+    return false;
+  });
 });
