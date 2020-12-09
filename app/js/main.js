87f80d92cdd8cf51b4e140729c1ee8ea781123f3
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-12-09 13:21:45 +0200

    burger-menu

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,14 @@
 $(function() {
   $(".header__burger-menu").on("click", function() {
     $(".header__menu").toggleClass("open-menu");
+    $(".header__burger-menu").addClass("hide-menu");
   });
 
   $(".header__link-item").on("click", function(event) {
     let anchor = $(this);
     $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top - 30 + "px"}, 777);
     $(".header__menu").removeClass("open-menu");
+    $(".header__burger-menu").removeClass("hide-menu");
 
     event.preventDefault();
     return false;
